4efaa8a5b42d34448541ce98f641df3b5aaa3b83
Piotr Jan Paturej <p.paturej@stud.elka.pw.edu.pl>
2020-12-29 17:56:25 +0100

    Add create_and_train_network function

--- main.py
+++ main.py
@@ -2,13 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.neural_network import MLPRegressor
 
-from neural_network.layers import Linear, ReLu
+from neural_network.layers import Linear, ReLu, Tanh, Sigmoid
 from neural_network.loss_functions import MSE
 from neural_network.neural_network import NeuralNetwork
 
 
 def aim_function(x):
-    return (x ** 3)
+    return x ** 3
 
 
 def create_samples(func, num):
@@ -17,27 +17,44 @@ def create_samples(func, num):
     return x_s, y_s
 
 
+def create_and_train_network():
+    x_train, y_train = create_samples(aim_function, 1000)
+    x_test, y_test = create_samples(aim_function, 100)
+    nn = NeuralNetwork(MSE())
+    first = Linear(1, 100)
+    first_activation = activation_function(100, 100)
+    second = Linear(100, 100)
+    second_activation = activation_function(100, 100)
+    third = Linear(100, 1)
+    nn.add(first)
+    nn.add(first_activation)
+    nn.add(second)
+    nn.add(second_activation)
+    nn.add(third)
+    nn.train(
+        x_train.T,
+        y_train.T,
+        learning_rate=learning_rate,
+        epochs=epochs,
+        minibatch_size=32,
+        calc_test_err=True,
+        x_test=x_test.T,
+        y_test=y_test.T,
+    )
+    return x_test, y_test
+
+
 def test_network():
     scores = []
     for i in range(20):
         np.random.seed = i + 1
-        x_train, y_train = create_samples(aim_function, 1000)
-        x_test, y_test = create_samples(aim_function, 100)
-        nn.train(
-            x_train.T,
-            y_train.T,
-            learning_rate=learning_rate,
-            epochs=epochs,
-            minibatch_size=32,
-            calc_test_err=True,
-            x_test=x_test.T,
-            y_test=y_test.T,
-        )
+        x_test, y_test = create_and_train_network()
         scores.append(nn.score(x_test.T, y_test.T))
     return np.mean(scores), np.std(scores)
 
 
-np.random.seed(38)
+np.random.seed(42)
+activation_function = Sigmoid
 x_train, y_train = create_samples(aim_function, 1000)
 
 x_test, y_test = create_samples(aim_function, 100)
@@ -46,9 +63,9 @@ learning_rate = 0.001
 epochs = 2000
 
 first = Linear(1, 100)
-first_activation = ReLu(100, 100)
+first_activation = activation_function(100, 100)
 second = Linear(100, 100)
-second_activation = ReLu(100, 100)
+second_activation = activation_function(100, 100)
 third = Linear(100, 1)
 
 nn = NeuralNetwork(MSE())
@@ -59,20 +76,27 @@ nn.add(second)
 nn.add(second_activation)
 nn.add(third)
 
-print('*' * 50 + 'Ours' + '*' * 50)
-errors, test_errors = nn.train(x_train.T, y_train.T, learning_rate=learning_rate, epochs=epochs, minibatch_size=64,
-                               calc_test_err=True, x_test=x_test.T, y_test=y_test.T)
+print("*" * 50 + "Ours" + "*" * 50)
+errors, test_errors = nn.train(
+    x_train.T,
+    y_train.T,
+    learning_rate=learning_rate,
+    epochs=epochs,
+    minibatch_size=64,
+    calc_test_err=True,
+    x_test=x_test.T,
+    y_test=y_test.T,
+)
 print(nn.score(x_test.T, y_test.T))
 
 avg_value, std_dev = test_network()
 print(f"Average loss function value: {avg_value}")
 print(f"Standard deviation: {std_dev}")
 
-print('*' * 50 + 'MLPRegressor' + '*' * 50)
+print("*" * 50 + "MLPRegressor" + "*" * 50)
 
-regr = MLPRegressor(solver='sgd', max_iter=epochs).fit(x_train, y_train.ravel())
+regr = MLPRegressor(solver="sgd", max_iter=epochs).fit(x_train, y_train.ravel())
 print(regr.score(x_test, y_test.ravel()))
-
 
 
 plt.xlabel("Epoch")
@@ -81,4 +105,5 @@ plt.plot(errors, label="Train data")
 plt.plot(test_errors, label="Test data")
 plt.legend()
 plt.grid()
+plt.savefig("graph")
 plt.show()
