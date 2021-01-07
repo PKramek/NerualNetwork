Fix errors calculation

neural_network/neural_network.py
@@ -58,7 +58,7 @@ class NeuralNetwork:
             yield x_train[:, indices[start: end]], y_train[:, indices[start: end]]
 
     def train(self, x_train: np.ndarray, y_train: np.ndarray, epochs: int, learning_rate: float,
-              minibatch_size: int, calc_test_err: bool = False, x_test: Optional[np.ndarray] = None,
+              minibatch_size: int, calc_errors: bool = False, x_test: Optional[np.ndarray] = None,
               y_test: Optional[np.ndarray] = None):
         assert isinstance(epochs, int) and epochs > 0, 'Number of epochs must be integer bigger than 0'
         assert isinstance(learning_rate, float) and learning_rate > 0, 'Learning rate must be float bigger than 0'
@@ -69,40 +69,36 @@ class NeuralNetwork:
         assert isinstance(y_train, np.ndarray)
         assert x_train.shape[1] == y_train.shape[1]
 
-        assert isinstance(calc_test_err, bool)
-        if calc_test_err:
+        assert isinstance(calc_errors, bool)
+        if calc_errors:
             assert isinstance(x_test, np.ndarray)
             assert isinstance(y_test, np.ndarray)
             assert x_test.shape[1] == y_test.shape[1]
 
             test_len = x_test.shape[1]
+            train_len = x_train.shape[1]
 
-        train_len = x_train.shape[1]
-
-        test_errors = []
-        errors = []
+            test_errors = []
+            errors = []
 
         for epoch in range(epochs):
-            if calc_test_err:
+            if calc_errors:
                 test_output = self.forward_propagation(x_test, no_grad=True)
                 test_errors.append(self._loss_function.loss(test_output, y_test) / test_len)
+                train_output = self.forward_propagation(x_train, no_grad=True)
+                errors.append(self._loss_function.loss(train_output, y_train) / train_len)
 
-            epoch_error = 0
+
             minibatch_generator = self.minibatch_generator(x_train, y_train, minibatch_size, True)
             for x, y in minibatch_generator:
                 output = self.forward_propagation(x)
-                epoch_error += self._loss_function.loss(output, y)
                 error_derivative = self._loss_function.derivative(output, y)
                 self.backward_propagation(error_derivative, learning_rate)
 
-            errors.append(epoch_error / train_len)
-
-
-        if calc_test_err:
-
+        if calc_errors:
             return errors, test_errors
 
-        return errors
+        return None
 
     def score(self, x_true: np.array, y_true: np.array):
         predictions = self.predict(x_true)